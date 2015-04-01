0fcebcd364c7fec59749471f4c8059596818ad0d
muffinman007 <cs70falls218@hotmail.com>
2015-04-01 13:00:12 -0700

    Added fields properties and ctor

--- NetworkInterface/NetworkInterface/Network.cs
+++ NetworkInterface/NetworkInterface/Network.cs
@@ -8,19 +8,29 @@ namespace NetworkInterface
 {
 	public class Network {
 		#region Fields
+		//switch
+		bool hasPackage;							// when network received data and finish de-serializing it this turn to true
 
-
+		// important data
+		int port;
 		#endregion Fields
 
 
 		#region Constructors
 
+		public Network(int port = 12345)
+		{
+			this.port = port;
+			hasPackage = false;
+		}
 
 		#endregion Constructors
 
 
 		#region Properties
-
+		public Package Package
+		{
+			if(
 
 		#endregion Properties
 
