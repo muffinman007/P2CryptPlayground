72f2b83350d351c9f7b811a48b346a7077717a6b
muffinman007 <cs70falls218@hotmail.com>
2015-04-01 12:26:29 -0700

    Set up structure of the Network class

--- NetworkInterface/NetworkInterface/Network.cs
+++ NetworkInterface/NetworkInterface/Network.cs
@@ -4,7 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetworkInterface {
-	public class Network{
+namespace NetworkInterface 
+{
+	public class Network {
+		#region Fields
+
+
+		#endregion Fields
+
+
+		#region Constructors
+
+
+		#endregion Constructors
+
+
+		#region Properties
+
+
+		#endregion Properties
+
+
+		#region Methods
+
+
+		#endregion Methods
 	}
 }
