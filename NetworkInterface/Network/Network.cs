651843a7008844af720b916f2c995686963498e2
muffinman007 <cs70falls218@hotmail.com>
2015-04-02 15:21:43 -0700

    Add additional projects, create a new project for Network.

---
+++ NetworkInterface/Network/Network.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkInterface 
+{
+	public class Network {
+		#region Fields
+		//switch
+		bool hasPackage;							// when network received data and finish de-serializing it this turn to true
+
+		// important data
+		int port;
+		#endregion Fields
+
+
+		#region Constructors
+
+		public Network(int port = 12345)
+		{
+			this.port = port;
+			hasPackage = false;
+		}
+
+		#endregion Constructors
+
+
+		#region Properties
+		public Package Package
+		{
+			if(
+
+		#endregion Properties
+
+
+		#region Methods
+
+
+		#endregion Methods
+	}
+}
---
+++ NetworkInterface/Network/Package.cs
@@ -0,0 +1,38 @@
+﻿#region Header
+
+/*
+ * The delivery package. Signature required.
+ * This class is the class that will be seralized before it get sent through the net.
+ *
+ * Can use properties if  you want, can change it around if like you but it must contain a PublicProfile and a byte[]
+ *
+ */
+
+#endregion Header
+
+using System;
+
+
+namespace NetworkInterface 
+{
+	[Serializable]
+	public class Package
+	{
+		#region Fields
+
+		public byte[] data;
+		public string user;				// change string to PublicProfile
+
+		#endregion Fields
+
+		#region Constructors
+
+		public Package(string user, byte[] data)
+		{
+			this.user = user;
+			this.data = data;
+		}
+
+		#endregion Constructors
+	}
+}
