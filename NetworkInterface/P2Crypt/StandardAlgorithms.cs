72de9803df33a84184054510c30e7e6c35a7d230
muffinman007 <cs70falls218@hotmail.com>
2015-04-02 18:40:14 -0700

    update solution structure

---
+++ NetworkInterface/P2Crypt/StandardAlgorithms.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.Cryptography;
+
+namespace P2Crypt
+{
+	/// <summary>
+	/// This class provide standard cryptos. 
+	/// 
+	/// All decisions like "should we use AES or DES for symmetric encryption"
+	/// are made here.
+	/// 
+	/// </summary>
+	public class StandardAlgorithms
+	{
+		#region Fields
+
+		/// <summary>
+		/// 128-256/64
+		/// </summary>
+		private static readonly int SymetricKeySizeInBits = 256;
+
+		#endregion Fields
+
+		#region Properties
+
+		private static int SymetricKeySizeInBytes
+		{
+			get { return SymetricKeySizeInBits >> 3; }
+		}
+
+		#endregion Properties
+
+		#region Methods
+
+		public static AsymmetricAlgorithm AsymetricAlgorithm()
+		{
+			RSACryptoServiceProvider rsaAlg = new RSACryptoServiceProvider();
+
+			return rsaAlg;
+		}
+
+		public static RSAParameters GenerateRSAParameters()
+		{
+			RSACryptoServiceProvider rsaAlg = new RSACryptoServiceProvider();
+
+			return rsaAlg.ExportParameters(true);
+		}
+
+		public static SymmetricAlgorithm SymmetricAlgorithmFromPassword(string password)
+		{
+			//Create key from password
+			byte[] salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+			Rfc2898DeriveBytes keyGen = new Rfc2898DeriveBytes(password, salt);
+
+			AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider()
+			{
+				Key = keyGen.GetBytes(SymetricKeySizeInBytes),
+			};
+			aesAlg.IV = keyGen.GetBytes(aesAlg.BlockSize >> 3);
+			return aesAlg;
+		}
+
+		#endregion Methods
+	}
+}
